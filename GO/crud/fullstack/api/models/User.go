7eac0d8b8d3372821e8a0d35f2abd3e2e2a8e244
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-26 15:22:26 +0530

    update a user

--- GO/crud/fullstack/api/models/User.go
+++ GO/crud/fullstack/api/models/User.go
@@ -50,42 +50,42 @@ func (u * User) Validate(action string) error {
 	switch strings.ToLower(action) {
 	case "update":
 		if u.Nickname == "" {
-			return errors.New("Required Nickname");
+			return errors.New("required nickname");
 		}
 		if u.Password == "" {
-			return errors.New("Required Password");
+			return errors.New("required password");
 		}
 		if u.Email == "" {
-			return errors.New("Required Email");
+			return errors.New("required email");
 		}
 		if err := checkmail.ValidateFormat(u.Email); err != nil {
-			return errors.New("Invalid Email");
+			return errors.New("invalid email");
 		}
 		return nil;
 	
 	case "login":
 		if u.Password == "" {
-			return errors.New("Required Password"); 
+			return errors.New("required password"); 
 		}	
 		if u.Email == "" {
-			return errors.New("Required Email");
+			return errors.New("required email");
 		}
 		if err := checkmail.ValidateFormat(u.Email); err != nil {
-			return errors.New("Invalid Email");
+			return errors.New("invalid email");
 		}
 		return nil;
 	default: 
 		if u.Nickname == "" {
-			return errors.New("Required Nickname");
+			return errors.New("required nickname");
 		}
 		if u.Password == "" {
-			return errors.New("Required Password");
+			return errors.New("required password");
 		}
 		if u.Email == "" {
-			return errors.New("Required Email");	
+			return errors.New("required email");	
 		}
 		if err := checkmail.ValidateFormat(u.Email); err != nil {
-			return errors.New("Invalid Email");
+			return errors.New("invalid email");
 		}
 		return nil;
 	}
@@ -93,6 +93,52 @@ func (u * User) Validate(action string) error {
 
 func (u *User) SaveUser(db *gorm.DB) (*User, error) {
 	err := db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil;
+}
+
+func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
+	users := []User{}
+	err := db.Debug().Model(&User{}).Limit(100).Find(&users).Error
+	if err != nil {
+		return &[]User{}, err
+	} 
+	return &users, err;
+}
+
+func (u *User) FindUserByID(db * gorm.DB, uid int32) (*User, error) {
+	err := db.Debug().Model(&User{}).Where("id = ?", uid).Take(&u).Error;
+	if err != nil {
+		return &User{}, err;
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &User{}, errors.New("User Not Found");
+	}
+	return u, err;
+}
+
+func (u * User) UpdateAUser(db *gorm.DB, uid uint32) (*User, error) {
+	
+	err := u.BeforeSave();
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	db = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&User{}).UpdateColumns(
+		map[string] interface{} {
+			"password": u.Password,
+			"nickname": u.Nickname,
+			"email": u.Email,
+			"update_at": time.Now(),
+		},
+	);
+	if db.Error != nil {
+		return &User{}, db.Error;
+	}
+	// Display the updated user
+	err = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&u).Error;
 	if err != nil {
 		return &User{}, err
 	}
