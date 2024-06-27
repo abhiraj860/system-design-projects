2c7949e42ed1dc43f2051a18e252a0dcb484202a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-27 22:35:43 +0530

    add create token

---
+++ GO/basic/api/auth/token.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func CreateToken(user_id uint32) (string, error) {
+	claims := jwt.MapClaims{};
+	claims["authorized"] = true;
+	claims["user_id"] = user_id;
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix();
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims);
+	return token.SignedString([]byte(os.Getenv("API_SECRET")));
+}
+
