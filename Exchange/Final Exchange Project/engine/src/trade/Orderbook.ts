76be7115cae223337539af922c464217d6e2f1f5
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-14 16:20:33 +0530

    project exchange

--- Exchange/Final Exchange Project/engine/src/trade/Orderbook.ts
+++ Exchange/Final Exchange Project/engine/src/trade/Orderbook.ts
@@ -87,11 +87,8 @@ export class Orderbook {
         const fills: Fill[] = [];
         let executedQty = 0;
 
-        for (let i = 0; i < this.asks.sort.length; i++) {
-            if (executedQty === order.quantity) {
-                break;
-            }
-            if (this.asks[i].price <= order.price) {
+        for (let i = 0; i < this.asks.length; i++) {
+            if (this.asks[i].price <= order.price && executedQty < order.quantity) {
                 const filledQty = Math.min((order.quantity - executedQty), this.asks[i].quantity);
                 executedQty += filledQty;
                 this.asks[i].filled += filledQty;
