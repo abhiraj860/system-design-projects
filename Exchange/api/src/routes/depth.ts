389ee71cbc84ce977aac550761cb89411dc2672f
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-17 16:09:31 +0530

    get depth

--- Exchange/api/src/routes/depth.ts
+++ Exchange/api/src/routes/depth.ts
@@ -1,18 +1,18 @@
 
-import { Router } from "express";
-import { RedisManager } from "../RedisManager";
-import { GET_DEPTH } from "../types";
+import { Router } from "express"; // import router from express
+import { RedisManager } from "../RedisManager"; // import redis manager
+import { GET_DEPTH } from "../types"; // import the type for get_depth
 
-export const depthRouter = Router();
+export const depthRouter = Router(); // create a depth router
 
-depthRouter.get("/", async (req, res) => {
-    const { symbol } = req.query;
-    const response = await RedisManager.getInstance().sendAndAwait({
-        type: GET_DEPTH,
-        data: {
-            market: symbol as string
+depthRouter.get("/", async (req, res) => { // get depth route
+    const { symbol } = req.query; // get the market symbol from the query
+    const response = await RedisManager.getInstance().sendAndAwait({ // send the request body and wait for the response
+        type: GET_DEPTH, // type of operation to be performed
+        data: { // data send to the redis queue to be processed by the engine
+            market: symbol as string  // send the symbol as string in the response body
         }
     });
 
-    res.json(response.payload);
+    res.json(response.payload); // get the response and send it as a client.
 });
