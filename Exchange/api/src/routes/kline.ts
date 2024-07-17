4e490d260520d9eaa7196abf840ea55dd3e23fb8
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-17 16:40:01 +0530

    api fin

--- Exchange/api/src/routes/kline.ts
+++ Exchange/api/src/routes/kline.ts
@@ -17,7 +17,7 @@ klineRouter.get("/", async (req, res) => { // get route for kline
 
     let query; // query array
     switch (interval) { // insert an interval
-        // get the data from resepective tables based on interval that were input
+        // get the data from resepective tables based on interval that were input note $1 and $2 is used for sql injection
         case '1m': // 1 minute interval
             query = `SELECT * FROM klines_1m WHERE bucket >= $1 AND bucket <= $2`;
             break;
@@ -30,20 +30,20 @@ klineRouter.get("/", async (req, res) => { // get route for kline
         default:
             return res.status(400).send('Invalid interval'); // return an invalid interval
     }
-
+    // Note: The $1 and $2 are present to avoid sql injection.
     try {
         //@ts-ignore
         const result = await pgClient.query(query, [new Date(startTime * 1000 as string), new Date(endTime * 1000 as string)]); // get the data for the specific start and end time
         res.json(result.rows.map(x => ({ // get the arrays from query and apply map
-            close: x.close, // show the closing price
-            end: x.bucket, // show the end bucket
-            high: x.high, // show the highes price
-            low: x.low, // show the low price in the period
-            open: x.open, // show the open price
-            quoteVolume: x.quoteVolume, // show the quote volume
-            start: x.start, // show the starting price 
-            trades: x.trades, // show the trades
-            volume: x.volume, // show the trade volume
+            close: x.close, // show the closing price in time interval bucket
+            end: x.bucket, // show the end time bucket for the interval
+            high: x.high, // show the highest price during the interval
+            low: x.low, // show the lowest price during the interval
+            open: x.open, // show the open price of the interval
+            quoteVolume: x.quoteVolume, // represent the volume of quote asset traded
+            start: x.start, // show the start time bucket for the interval 
+            trades: x.trades, // show the number of trades during the interval
+            volume: x.volume, // show trading volume in the bucket
         })));
     } catch (err) {
         console.log(err); // print out the error 
