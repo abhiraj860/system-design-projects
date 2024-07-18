8feb337a7f785bee1df3fb57db1578951c13bdc3
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-18 22:41:31 +0530

    ws done

--- Exchange/ws/src/SubscriptionManager.ts
+++ Exchange/ws/src/SubscriptionManager.ts
@@ -1,63 +1,66 @@
-import { RedisClientType, createClient } from "redis";
-import { UserManager } from "./UserManager";
+// subscriptions manager
+import { RedisClientType, createClient } from "redis"; // import redis client and its type
+import { UserManager } from "./UserManager"; // import the user manager
 
-export class SubscriptionManager {
-    private static instance: SubscriptionManager;
-    private subscriptions: Map<string, string[]> = new Map();
-    private reverseSubscriptions: Map<string, string[]> = new Map();
-    private redisClient: RedisClientType;
+export class SubscriptionManager { // create a subscription manager class
+    private static instance: SubscriptionManager; // create a subscriptions manager instance
+    private subscriptions: Map<string, string[]> = new Map(); // create a map of that maps from string to array of string 
+    private reverseSubscriptions: Map<string, string[]> = new Map(); // create a reverse subscription map
+    private redisClient: RedisClientType; // create a redis client for the user manager
 
-    private constructor() {
-        this.redisClient = createClient();
-        this.redisClient.connect();
+    private constructor() { // create a constructor function
+        this.redisClient = createClient(); // create a redis client
+        this.redisClient.connect(); // connect to the redis client
     }
 
-    public static getInstance() {
-        if (!this.instance)  {
-            this.instance = new SubscriptionManager();
+    // singleton pattern
+    public static getInstance() { 
+        if (!this.instance)  { // if the instance is not present
+            this.instance = new SubscriptionManager(); // create a new instance
         }
-        return this.instance;
+        return this.instance; // return the instance
     }
 
-    public subscribe(userId: string, subscription: string) {
-        if (this.subscriptions.get(userId)?.includes(subscription)) {
-            return
+    public subscribe(userId: string, subscription: string) { // for the subscription
+        if (this.subscriptions.get(userId)?.includes(subscription)) { // if the subscription channel is included in the userId array then
+            return // then return the object
         }
+        // if the subscription is not present in the userId array then...
+        this.subscriptions.set(userId, (this.subscriptions.get(userId) || []).concat(subscription)); // at the subscription to the user map
+        this.reverseSubscriptions.set(subscription, (this.reverseSubscriptions.get(subscription) || []).concat(userId)); // this is the reverse subscription where for each subscription the userId is added
+        if (this.reverseSubscriptions.get(subscription)?.length === 1) { // if the subscription has atleast one user then connect to the redis callback handler
 
-        this.subscriptions.set(userId, (this.subscriptions.get(userId) || []).concat(subscription));
-        this.reverseSubscriptions.set(subscription, (this.reverseSubscriptions.get(subscription) || []).concat(userId));
-        if (this.reverseSubscriptions.get(subscription)?.length === 1) {
-
-            this.redisClient.subscribe(subscription, this.redisCallbackHandler);
+            this.redisClient.subscribe(subscription, this.redisCallbackHandler); // subscribe to the redis pubsub on the subscription channel
         }
     }
 
-    private redisCallbackHandler = (message: string, channel: string) => {
-        const parsedMessage = JSON.parse(message);
+    private redisCallbackHandler = (message: string, channel: string) => { // this is the redis callback handler
+        const parsedMessage = JSON.parse(message); // get the parsed message in parsedMessage and process it accordingly 
         this.reverseSubscriptions.get(channel)?.forEach(s => UserManager.getInstance().getUser(s)?.emit(parsedMessage));
+        // get the channel and for each of the user subscribed to the channel emit the parsed message once subscribed
     }
 
-    public unsubscribe(userId: string, subscription: string) {
-        const subscriptions = this.subscriptions.get(userId);
-        if (subscriptions) {
-            this.subscriptions.set(userId, subscriptions.filter(s => s !== subscription));
+    public unsubscribe(userId: string, subscription: string) { // unsubscribe the user from the websocket
+        const subscriptions = this.subscriptions.get(userId); // get the list of user id from the subscriptions map
+        if (subscriptions) { // if the subscriptions are present
+            this.subscriptions.set(userId, subscriptions.filter(s => s !== subscription)); // remove the subscription from the subscription map
         }
-        const reverseSubscriptions = this.reverseSubscriptions.get(subscription);
-        if (reverseSubscriptions) {
-            this.reverseSubscriptions.set(subscription, reverseSubscriptions.filter(s => s !== userId));
-            if (this.reverseSubscriptions.get(subscription)?.length === 0) {
-                this.reverseSubscriptions.delete(subscription);
-                this.redisClient.unsubscribe(subscription);
+        const reverseSubscriptions = this.reverseSubscriptions.get(subscription); // now get the subscriptions users from the reverse subscriptions
+        if (reverseSubscriptions) { // if the reverseSubscriptions are present
+            this.reverseSubscriptions.set(subscription, reverseSubscriptions.filter(s => s !== userId)); // then remove that user from the unsubscription array
+            if (this.reverseSubscriptions.get(subscription)?.length === 0) { // if the length of the reverse subscription is zero
+                this.reverseSubscriptions.delete(subscription); // delete the subscription reverse subscription
+                this.redisClient.unsubscribe(subscription); // unsubscribe from redis client from the specific channel
             }
         }
     }
 
-    public userLeft(userId: string) {
-        console.log("user left " + userId);
-        this.subscriptions.get(userId)?.forEach(s => this.unsubscribe(userId, s));
+    public userLeft(userId: string) { // this is for the userleft
+        console.log("user left " + userId); // print out the userId 
+        this.subscriptions.get(userId)?.forEach(s => this.unsubscribe(userId, s)); // when the user leaves, for each of the channels subscribed unsubscribe them
     }
     
-    getSubscriptions(userId: string) {
-        return this.subscriptions.get(userId) || [];
+    getSubscriptions(userId: string) { // this mehod is used to get the subscription
+        return this.subscriptions.get(userId) || []; // return the subscriptions of the userId
     }
 }
