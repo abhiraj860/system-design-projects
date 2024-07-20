993e90600e913a98b76c8eaba6cc4a1b4a076523
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-20 08:16:32 +0530

    add type

--- Exchange/engine/src/types/index.ts
+++ Exchange/engine/src/types/index.ts
@@ -1,2 +1,2 @@
-export const TRADE_ADDED = "TRADE_ADDED";
-export const ORDER_UPDATE = "ORDER_UPDATE";
+export const TRADE_ADDED = "TRADE_ADDED"; // trade added type
+export const ORDER_UPDATE = "ORDER_UPDATE"; // order update type
