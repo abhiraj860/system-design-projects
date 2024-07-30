5ce17bc9549277e49207a5218de24fec92047033
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-30 17:28:07 +0530

    change button

--- Zappier/zap/frontend/components/Hero.tsx
+++ Zappier/zap/frontend/components/Hero.tsx
@@ -8,23 +8,26 @@ export const Hero = () => {
     const router = useRouter();
     return <div>
         <div className="flex justify-center">
-            <div className="text-5xl font-bold font-semibold text-center pt-8 max-w-xl">
+            <div className="text-5xl font-bold text-center pt-8 max-w-xl">
                 Automate as fast as you can type    
             </div>
         </div>
         <div className="flex justify-center pt-2">
-            <div className="text-xl font-bold font-normal text-center pt-8 max-w-2xl">
+            <div className="text-xl font-bold  text-center pt-8 max-w-2xl">
                 AI gives you automation superpowers, and Zapier puts them to work. Pairing AI and Zapier helps you turn ideas into workflows and bots that work for you.
             </div>
         </div>
 
         <div className="flex justify-center pt-4">
             <div className="flex">
+                <div>
                 <PrimaryButton onClick={() => {
                     router.push("/signup")
-                }} size="big">Get Started free</PrimaryButton>
+                }} size="big"><span className="text-base">Get Started free</span>
+                </PrimaryButton>
+                </div>
                 <div className="pl-4">
-                    <SecondaryButton  onClick={() => {}} size="big">Contact Sales</SecondaryButton>
+                    <SecondaryButton  onClick={() => {}} size="big"> <span className="text-base font-bold">Contact Sales</span></SecondaryButton>
                 </div>
             </div>
         </div>
