0ad2e8906c72c8ffec30107f3bf5ae5fdd281698
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-08-01 15:02:01 +0530

    remove

--- CMS/cms-main/src/components/AppbarAuth.tsx
+++ CMS/cms-main/src/components/AppbarAuth.tsx
@@ -2,12 +2,10 @@
 
 import { signIn } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
-//import { useRouter } from 'next/navigation';
 import { Button } from './ui/button';
 
 export const AppbarAuth = ({ isInMenu = false }: { isInMenu?: boolean }) => {
   const session = useSession();
-  //const router = useRouter();
 
   return (
     !session?.data?.user && (
