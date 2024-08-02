d0bb8e4e7f52e184ba95539b993822a39858c7b1
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-08-02 07:54:25 +0530

    add servers

---
+++ Assignment/dist/server_1/index_1.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const PORT = 8080;
+const wss = new ws_1.WebSocketServer({ port: PORT });
+wss.on('connection', function connection(ws) {
+    console.log(PORT, " connected");
+    ws.on('error', console.error);
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    ws.send('something');
+});
+console.log("Listenining on PORT ", PORT);
